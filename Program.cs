7b898dfb44019a30f5b2d44e81c767bbf7e400f6
‪Avi Brodetsky‬‏ <95278057+avib1997@users.noreply.github.com>
2023-10-25 10:43:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        string fileName = "../data.txt";
+        try
+        {
+            string content = await ReadFileAsync(fileName);
+            if (content != null)
+            {
+                Console.WriteLine($"Content from {fileName}: {content}");
+            }
+            else
+            {
+                Console.WriteLine($"File {fileName} is empty.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadLine(); // Wait for user input to exit
+    }
+
+    static async Task<string> ReadFileAsync(string fileName)
+    {
+        using (StreamReader reader = new StreamReader(fileName))
+        {
+            return await reader.ReadToEndAsync();
+        }
+    }
+}
